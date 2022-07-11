9fd2af8cf9706c8fe69d1847d2a02591e8de68f3
LOfangQuanZachary <88233381+LOfangQuanZachary@users.noreply.github.com>
2022-07-11 12:14:46 +0800

    added more functions

--- Funni.py
+++ Funni.py
@@ -4,23 +4,36 @@ import time
 import serial
 beginButton = 5
 serialInput = ""
-def setUpCode():
-    GPIO.pinMode(5, GPIO.OUTPUT)
-    if __name__ == '__main__':
-        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        ser.reset_input_buffer()
-    GPIO.cleanup()
-def selfDrivingCode(gyroAngle, currentMotorSpeedA, currentMotorSpeedB):
-    pass
+#start of serial transmission setup
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+#Serial read/write function
 def serialTransmission(toTransmit):
     #function will return any inputs
     if serial.in_waiting > 0:
-        serialInput = serial.readline().decode("utf-8").rstrip()
+        serialInput = ser.readline().decode("utf-8").rstrip()
     else:
-        serianInput = ""
-    serial.write(toTransmit + "/n".encode("utf-8"))
+        serialInput = ""
+    if toTransmit != "":
+        ser.write(toTransmit + "/n".encode("utf-8"))
+    ser.reset_input_buffer()
     return serialInput
-
+#setUp and initialization code
+def setUpCode():
+    GPIO.pinMode(5, GPIO.OUTPUT)
+    GPIO.cleanup()
+    serialTransmission("Begin")
+    while serialTransmission("") != "StartingRun":
+        time.sleep(0.01)
+#self driving functions
+def selfDrivingCode(gyroAngle, currentMotorSpeedA, currentMotorSpeedB):
+    pass
+#shut down code
+def shutDown():
+    serialTransmission("kill")
+#main body of code
 def main():
     setUpCode()
+main()
 
